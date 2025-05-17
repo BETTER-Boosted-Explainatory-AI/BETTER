aecee58dedbb90a374fad46ecf9aa527d54c873b
Adi Levi <104142857+adi-git33@users.noreply.github.com>
2025-05-17 15:54:12 +0300

    feat: added debugger utils

---
+++ utilss/debug_utils.py
@@ -0,0 +1,10 @@
+import logging
+import traceback
+
+# Set up logging with a more detailed format
+logging.basicConfig(
+    level=logging.DEBUG,  # Changed to DEBUG for more verbose output
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
