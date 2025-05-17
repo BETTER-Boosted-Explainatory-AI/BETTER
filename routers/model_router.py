df842c888bd41b27edc68042fe74bc6c9f5a89ef
Adi Levi <104142857+adi-git33@users.noreply.github.com>
2025-05-17 19:17:25 +0300

    fix: get all models

--- routers/model_router.py
+++ routers/model_router.py
@@ -3,8 +3,29 @@ from request_models.model_model import ModelRequest, ModelResult
 from services.users_service import get_current_session_user
 from utilss.classes.user import User
 from services.models_service import get_user_models_info
+from typing import List
 
 model_router = APIRouter()
+
+@model_router.get(
+    "/models", 
+    response_model=List[ModelResult],  # <-- Return a plain list
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "Resource not found"},
+        status.HTTP_422_UNPROCESSABLE_ENTITY: {"description": "Validation error"},
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {"description": "Internal server error"}
+    }
+)
+async def get_model_info(current_user: User = Depends(get_current_session_user)) -> List[ModelResult]:
+    models_info = get_user_models_info(current_user, None)
+    
+    if models_info is None:
+        raise HTTPException(status_code=404, detail="Model not found")
+    
+    return models_info
+
+
 
 @model_router.post(
     "/models", 
