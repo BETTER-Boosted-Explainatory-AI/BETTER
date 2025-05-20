7c28c39be16e98e1367a17442531e6654fa40d98
YasminAdler <118344195+YasminAdler@users.noreply.github.com>
2025-05-20 13:42:11 +0300

    nothiing

--- services/dataset_service.py
+++ services/dataset_service.py
@@ -6,31 +6,6 @@ from utilss.enums.datasets_enum import DatasetsEnum
 import shutil
 from utilss.s3_datasets_connector.s3_dataset_loader import S3DatasetLoader
 
-
-# def _get_dataset_config(dataset_str: str) -> Dict[str, Any]:
-#     """Get dataset configuration based on dataset string."""
-#     if dataset_str == DatasetsEnum.CIFAR100.value:
-#         return CIFAR100_INFO
-#     elif dataset_str == DatasetsEnum.IMAGENET.value:
-#         return IMAGENET_INFO
-#     else:
-#         raise ValueError(f"Invalid dataset: {dataset_str}")
-
-# def _load_dataset(dataset_config: Dict[str, Any]):
-#     """Load the dataset based on configuration."""
-#     dataset = DatasetFactory.create_dataset(dataset_config["dataset"])
-#     dataset.load(dataset_config["dataset"])
-    
-#     return dataset
-
-# def _get_dataset_labels(dataset_str: str) -> List[str]:
-#     """Get dataset labels based on dataset string."""
-#     if dataset_str == DatasetsEnum.CIFAR100.value:
-#         return CIFAR100_INFO["labels"]
-#     elif dataset_str == DatasetsEnum.IMAGENET.value:
-#         return IMAGENET_INFO["labels"]
-#     else:
-#         raise ValueError(f"Invalid dataset: {dataset_str}")
     
 def _get_dataset_config(dataset_str: str) -> Dict[str, Any]:
     """Get dataset configuration based on dataset string."""
