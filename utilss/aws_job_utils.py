bd43328055f242858eb7a54140b72b7bf069c328
Adi Levi <104142857+adi-git33@users.noreply.github.com>
2025-05-26 15:17:56 +0300

    fix: (not final) started matching the aws job to start the batch

--- utilss/aws_job_utils.py
+++ utilss/aws_job_utils.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 
 # def submit_nma_batch_job(user, model_id, dataset, graph_type, min_confidence, top_k):
-def submit_nma_batch_job(user_id):
+def submit_nma_batch_job(user_id, model_id, graph_type):
     batch = boto3.client('batch', region_name=os.getenv("AWS_REGION"),
                         aws_access_key_id = os.getenv("AWS_JOBS_ACCESS_KEY_ID"),
                         aws_secret_access_key = os.getenv("AWS_JOBS_SECRET_ACCESS_KEY")) 
@@ -13,25 +13,18 @@ def submit_nma_batch_job(user_id):
     job_name = f"nma-job-{uuid.uuid4()}"
     job_queue = os.getenv("JOB_QUEUE_NAME")
     job_definition = os.getenv("JOB_DEFINITION_NAME")
-
-    # parameters = {
-    #     "user_id": user.id,
-    #     "model_id": model_id,
-    #     "dataset": dataset,
-    #     "graph_type": graph_type,
-    #     "min_confidence": str(min_confidence),
-    #     "top_k": str(top_k),
-    #     "timestamp": datetime.now(datetime.timezone.utc)
-    # }
     
     parameters = {
         "user_id": user_id,
+        "model_id": model_id,
+        "graph_type": graph_type
     }
 
     response = batch.submit_job(
         jobName=job_name,
         jobQueue=job_queue,
         jobDefinition=job_definition,
+        parameters=parameters,
     )
 
     if response.get("jobId"):
