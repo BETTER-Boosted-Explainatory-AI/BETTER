b2795301580927f2d3a1a6ec1c6279c8d711a1fa
MoranSin <nurixhbh@gmail.com>
2025-05-30 19:56:09 +0300

    fix: main for production

--- routers/users_router.py
+++ routers/users_router.py
@@ -85,10 +85,8 @@ def login_user(user_create_request: UserCreateRequest, response: Response) -> di
             key="session_token",
             value=id_token,
             httponly=True,      # Prevents JS access
-            secure=False,      # Only for local testing, set to True in production!
-            # secure=True,        # Only sent over HTTPS
-            # samesite="none",     # Adjust as needed
-            samesite="lax",      # Adjust as needed
+            secure=True,        # Only sent over HTTPS
+            samesite="none",     # Adjust as needed
             max_age=900        # 1 hour, adjust as needed
         )
 
@@ -96,10 +94,8 @@ def login_user(user_create_request: UserCreateRequest, response: Response) -> di
             key="refresh_token",
             value=refresh_token,
             httponly=True,      # Prevents JS access
-            secure=False,      # Only for local testing, set to True in production!
-            # secure=True,        # Only sent over HTTPS
-            # samesite="none",     # Adjust as needed
-            samesite="lax",      # Adjust as needed
+            secure=True,        # Only sent over HTTPS
+            samesite="none",     # Adjust as needed
             max_age=7*24*3600      # 7 days, adjust as needed
         )
 
@@ -110,10 +106,8 @@ def login_user(user_create_request: UserCreateRequest, response: Response) -> di
             key="user_id",
             value=user.user_id,
             httponly=True,      # Prevents JS access
-            secure=False,      # Only for local testing, set to True in production!
-            # secure=True,        # Only sent over HTTPS
-            # samesite="none",     # Adjust as needed
-            samesite="lax",      # Adjust as needed
+            secure=True,        # Only sent over HTTPS
+            samesite="none",     # Adjust as needed
             max_age=7*24*3600      # 7 days, adjust as needed
         )
 
@@ -178,10 +172,8 @@ def refresh_user_session(request: Request, response: Response):
             key="session_token",
             value=id_token,
             httponly=True,
-            # secure=True,  # Set to True in production!
-            secure=False,  # Only for local testing, set to True in production!
-            # samesite="none",
-            samesite="lax",  # Adjust as needed
+            secure=True,  # Set to True in production!
+            samesite="none",
             max_age=900
         )
 
@@ -190,10 +182,8 @@ def refresh_user_session(request: Request, response: Response):
                 key="refresh_token",
                 value=refresh_token,
                 httponly=True,
-                # secure=True,  # Set to True in production!
-                secure=False,  # Only for local testing, set to True in production!
-                # samesite="none",
-                samesite="lax",  # Adjust as needed
+                secure=True,  # Set to True in production!
+                samesite="none",
                 max_age=7*24*3600  # 7 days
             )
 
